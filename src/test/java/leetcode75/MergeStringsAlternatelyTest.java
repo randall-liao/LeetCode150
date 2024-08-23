e799c0c749d9bf3131c98f3e66b69377c5ee4df9
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2024-08-22 23:57:05 -0400

    1768. Merge Strings Alternately solved

---
+++ src/test/java/leetcode75/MergeStringsAlternatelyTest.java
@@ -0,0 +1,52 @@
+package leetcode75;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class MergeStringsAlternatelyTest {
+
+    private final MergeStringsAlternately mergeStringsAlternatelyDefault;
+
+
+    MergeStringsAlternatelyTest() {
+        mergeStringsAlternatelyDefault = new MergeStringsAlternately();
+    }
+
+    @Test
+    void mergeAlternately_nullInput_throwException() {
+        assertThrows(NullPointerException.class, () -> {
+            mergeStringsAlternatelyDefault.mergeAlternately(null, null);
+        });
+
+        assertThrows(NullPointerException.class, () -> {
+            mergeStringsAlternatelyDefault.mergeAlternately(null, "1");
+        });
+
+        assertThrows(NullPointerException.class, () -> {
+            mergeStringsAlternatelyDefault.mergeAlternately("2", null);
+        });
+    }
+
+    @Test
+    void mergeAlternately_equalLengthInput() {
+        assertEquals("apbqcr",mergeStringsAlternatelyDefault.mergeAlternately("abc","pqr"));
+    }
+
+    @Test
+    void mergeAlternately_emptyInput() {
+        assertEquals("",mergeStringsAlternatelyDefault.mergeAlternately("",""));
+    }
+
+    @Test
+    void mergeAlternately_leftShortInput() {
+        assertEquals("apbqrs",mergeStringsAlternatelyDefault.mergeAlternately("ab","pqrs"));
+    }
+
+    @Test
+    void mergeAlternately_rightShortInput() {
+        assertEquals("apbqcd",mergeStringsAlternatelyDefault.mergeAlternately("abcd","pq"));
+    }
+}
