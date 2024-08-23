cd3340c02168307b59410eb9a04fa7d6eed74c7c
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2024-08-22 23:35:34 -0400

    Refactor Junit4 to Junit5

--- src/test/java/MergeSortedArrayTest.java
+++ src/test/java/MergeSortedArrayTest.java
@@ -1,7 +1,7 @@
-import org.junit.Assert;
-import org.junit.Test;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class MergeSortedArrayTest {
 
