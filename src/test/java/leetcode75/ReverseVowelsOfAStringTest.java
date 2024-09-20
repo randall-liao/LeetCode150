8b2465a64e6e8e3c89e73044194c7f3c8a20ea5a
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2024-09-20 15:00:46 -0400

    feat: Finish 345. Reverse Vowels of a String and its unit tests

---
+++ src/test/java/leetcode75/ReverseVowelsOfAStringTest.java
@@ -0,0 +1,52 @@
+package leetcode75;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ReverseVowelsOfAStringTest {
+
+    private final ReverseVowelsOfAString reverseVowelsOfAString = new ReverseVowelsOfAString();
+
+    @Test
+    void reverseVowels_nullInput_expectedIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> reverseVowelsOfAString.reverseVowels(null));
+    }
+
+    @Test
+    void reverseVowels_nonAsciiInput_expectedIllegalArgumentException() {
+        String inputString = "测试";
+        assertThrows(IllegalArgumentException.class, () -> reverseVowelsOfAString.reverseVowels(inputString));
+    }
+
+    @Test
+    void reverseVowels_emptyStringInput_expectedEmptyStringReturn() {
+        String inputString = "";
+        String expectedOutputString = "";
+        assertEquals(reverseVowelsOfAString.reverseVowels(inputString), expectedOutputString);
+    }
+
+    @Test
+    void reverseVowels_mixedUpperAndLowerCaseIceCreAmInput_expectedAceCreImStringReturn() {
+        String inputString = "IceCreAm";
+        String expectedOutputString = "AceCreIm";
+        assertEquals(reverseVowelsOfAString.reverseVowels(inputString), expectedOutputString);
+    }
+
+    @Test
+    void reverseVowels_allLowerCaseLeetcodeInput_expectedLeotcedeStringReturn() {
+        String inputString = "leetcode";
+        String expectedOutputString = "leotcede";
+        assertEquals(reverseVowelsOfAString.reverseVowels(inputString), expectedOutputString);
+    }
+
+    @Test
+    void reverseVowels_allUpperCaseLeetcodeInput_expectedLeotcedeStringReturn() {
+        String inputString = "LEETCODE";
+        String expectedOutputString = "LEOTCEDE";
+        assertEquals(reverseVowelsOfAString.reverseVowels(inputString), expectedOutputString);
+    }
+
+
+}
