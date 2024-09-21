62035267c16b38962d724524857c0327263e7c42
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2024-09-20 21:05:34 -0400

    feat: 151. Reverse Words in a String and its unit tests

---
+++ src/main/java/leetcode75/ReverseWordsInAString.java
@@ -0,0 +1,37 @@
+package leetcode75;
+
+
+public class ReverseWordsInAString {
+
+    public static final char WORD_SEPARATOR = ' ';
+
+    public String reverseWords(String s) {
+        if (s == null) throw new IllegalArgumentException("Null input");
+        if (s.isBlank()) return "";
+        StringBuilder resultStringBuilder = new StringBuilder(s.length());
+        int lastSeparatorIndex = s.length();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            // iterate backward to find space in the string
+            if (WORD_SEPARATOR == s.charAt(i)) {
+                if (lastSeparatorIndex - i > 1) {
+                    // When there is no consecutive space, append all char between this and last space
+                    resultStringBuilder.append(s, i + 1, lastSeparatorIndex);
+                    resultStringBuilder.append(" ");
+                }
+                lastSeparatorIndex = i;
+
+            }
+        }
+        if (WORD_SEPARATOR != s.charAt(0)) {
+            // processing the first word of the string
+            resultStringBuilder.append(s, 0, lastSeparatorIndex);
+        }
+        if (WORD_SEPARATOR == resultStringBuilder.charAt(resultStringBuilder.length() - 1)) {
+            // trim the last space
+            resultStringBuilder.deleteCharAt(resultStringBuilder.length() - 1);
+        }
+        return resultStringBuilder.toString();
+    }
+
+
+}
---
+++ src/test/java/leetcode75/ReverseWordsInAStringTest.java
@@ -0,0 +1,49 @@
+package leetcode75;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ReverseWordsInAStringTest {
+
+    private final ReverseWordsInAString reverseWordsInAString = new ReverseWordsInAString();
+
+    @Test
+    void reverseWords_nullInput_expectedIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> reverseWordsInAString.reverseWords(null));
+    }
+
+    @Test
+    void reverseWords_emptyString_expectedEmptyString() {
+        assertEquals("", reverseWordsInAString.reverseWords(""));
+    }
+
+    @Test
+    void reverseWords_blankString_expectedEmptyString() {
+        assertEquals("", reverseWordsInAString.reverseWords("    "));
+    }
+
+    @Test
+    void reverseWords_normalSentence_expectedReversedSentence() {
+        assertEquals("blue is sky the", reverseWordsInAString.reverseWords("the sky is blue"));
+    }
+
+
+    @Test
+    void reverseWords_multipleBlankSpaceBetweenWords_expectedReversedSentence() {
+        assertEquals("example good a", reverseWordsInAString.reverseWords("a good   example"));
+    }
+
+    @Test
+    void reverseWords_multipleBlankSpaceAtHeadAndTail_expectedReversedSentence() {
+        assertEquals("blue is sky the", reverseWordsInAString.reverseWords("   the sky is blue  "));
+    }
+
+    @Test
+    void reverseWords_oneBlankSpaceAtHeadAndTail_expectedReversedSentence() {
+        assertEquals("blue is sky the", reverseWordsInAString.reverseWords(" the sky is blue "));
+    }
+
+
+}
