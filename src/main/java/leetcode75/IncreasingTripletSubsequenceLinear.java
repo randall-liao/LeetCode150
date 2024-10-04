4496069eb1f4eef06d0052781ac17b4d56fe63ed
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2024-10-04 16:31:57 -0400

    feat: Finish runtime O(N), mem O(1) algorithm for 334. Increasing Triplet Subsequence

---
+++ src/main/java/leetcode75/IncreasingTripletSubsequenceLinear.java
@@ -0,0 +1,22 @@
+package leetcode75;
+
+public class IncreasingTripletSubsequenceLinear {
+
+  public static boolean increasingTriplet(int[] nums) {
+    if (nums == null || nums.length < 3) {
+      return false;
+    }
+    Integer firstIntInTriplet = Integer.MAX_VALUE;
+    Integer secondIntInTriplet = Integer.MAX_VALUE;
+    for (int num : nums) {
+      if (num <= firstIntInTriplet) {
+        firstIntInTriplet = num;
+      } else if (num <= secondIntInTriplet) {
+        secondIntInTriplet = num;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
