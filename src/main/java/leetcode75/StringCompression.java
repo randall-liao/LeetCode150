7c5bf3f3e4644f830c02323cd37a1b1f1ada4e93
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2024-10-07 02:32:04 -0400

    feat: Finish 443. String Compression and its tests.

---
+++ src/main/java/leetcode75/StringCompression.java
@@ -0,0 +1,38 @@
+package leetcode75;
+
+public class StringCompression {
+
+  public static int compress(char[] chars) {
+    if (chars == null || chars.length == 0) {
+      return 0;
+    }
+    StringBuilder resultStringBuilder = new StringBuilder();
+    char previousChar = chars[0];
+    int repeatCharCounterInt = 0;
+    for (char ch : chars) {
+      // iterate through char array
+      if (previousChar == ch) {
+        // repeating Char
+        repeatCharCounterInt++;
+      } else {
+        // find a different char, write result into the StringBuilder and reset
+        add(previousChar, repeatCharCounterInt, resultStringBuilder);
+        previousChar = ch;
+        repeatCharCounterInt = 1;
+      }
+    }
+    add(chars[chars.length - 1], repeatCharCounterInt, resultStringBuilder);
+    for (int i = 0; i < resultStringBuilder.length(); i++) {
+      // Write the result back into chars array as the prompt required.
+      chars[i] = resultStringBuilder.charAt(i);
+    }
+    return resultStringBuilder.length();
+  }
+
+  private static void add(char ch, int repeatCharCounterInt, StringBuilder resultStringBuilder) {
+    resultStringBuilder.append(ch);
+    if (repeatCharCounterInt > 1) {
+      resultStringBuilder.append(repeatCharCounterInt);
+    }
+  }
+}
---
+++ src/test/java/leetcode75/StringCompressionTest.java
@@ -0,0 +1,64 @@
+package leetcode75;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringCompressionTest {
+
+  @Test
+  void compress_nullInput_expectedZero() {
+    char[] inputCharArray = null;
+    int expectedInt = 0;
+    assertEquals(expectedInt, StringCompression.compress(inputCharArray));
+  }
+
+  @Test
+  void compress_emptyInput_expectedZero() {
+    char[] inputCharArray = {};
+    int expectedInt = 0;
+    assertEquals(expectedInt, StringCompression.compress(inputCharArray));
+  }
+
+  @Test
+  void compress_example1() {
+    char[] inputCharArray = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+    int expectedInt = 6;
+    assertEquals(expectedInt, StringCompression.compress(inputCharArray));
+  }
+
+  @Test
+  void compress_example2() {
+    char[] inputCharArray = {'a'};
+    int expectedInt = 1;
+    assertEquals(expectedInt, StringCompression.compress(inputCharArray));
+  }
+
+  @Test
+  void compress_example3() {
+    char[] inputCharArray = {'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'};
+    int expectedInt = 4;
+    assertEquals(expectedInt, StringCompression.compress(inputCharArray));
+  }
+
+  @Test
+  void compress_sameChar_expectedOneCharWithRepeatTimes() {
+    char[] inputCharArray = {'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'};
+    int expectedInt = 3;
+    assertEquals(expectedInt, StringCompression.compress(inputCharArray));
+  }
+
+  @Test
+  void compress_AlternateTwoRepeatChar_expect() {
+    char[] inputCharArray = {'a', 'a', 'b', 'b'};
+    int expectedInt = 4;
+    assertEquals(expectedInt, StringCompression.compress(inputCharArray));
+  }
+
+  @Test
+  void compress_example4() {
+    char[] inputCharArray = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+    int expectedInt = 6;
+    assertEquals(expectedInt, StringCompression.compress(inputCharArray));
+  }
+}
