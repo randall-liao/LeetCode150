423d58d3ba365f56332a16b90385a44e025cfbcb
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2024-10-08 17:41:43 -0400

    feat: finish Selection Sort and its tests.

--- src/main/java/core/algorithms/SelectionSort.java
+++ src/main/java/core/algorithms/SelectionSort.java
@@ -1,10 +1,24 @@
 package core.algorithms;
 
-import java.util.Arrays;
-
 public class SelectionSort {
 
   public static int[] sort(int[] nums) {
-    return null;
+    if (nums == null || nums.length == 0) {
+      return nums;
+    }
+    for (int i = 0; i < nums.length; i++) {
+      int currentMinIndex = i;
+      int currentMinInt = nums[i];
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[j] < currentMinInt) {
+          currentMinInt = nums[j];
+          currentMinIndex = j;
+        }
+      }
+      int temp = nums[i];
+      nums[i] = nums[currentMinIndex];
+      nums[currentMinIndex] = temp;
+    }
+    return nums;
   }
 }
