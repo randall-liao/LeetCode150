5c4b8396089e0b51e75c1ac55ca990930c6b62ac
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2024-11-07 17:49:07 -0500

    feat: LeetCode 11. Container With Most Water two pointer/ greedy implementation and tests

--- src/main/java/leetcode75/twopointer/ContainerWithMostWater.java
+++ src/main/java/leetcode75/twopointer/ContainerWithMostWater.java
@@ -1,8 +1,7 @@
 package leetcode75.twopointer;
 
-import java.util.ArrayList;
-
 public class ContainerWithMostWater {
+
 
   private static int min(int x, int y) {
     if (x > y) {
@@ -18,15 +17,19 @@ public class ContainerWithMostWater {
       return -1;
     }
     int maxArea = Integer.MIN_VALUE;
-    ArrayList<Integer> resultIntList = new ArrayList<>();
-    for (int i = 0; i < height.length - 1; i++) {
-      for (int j = height.length - 1; j > i; j--) {
-        int currentHeight = min(height[i], height[j]);
-        int currentLength = j - i;
-        int currentArea = currentHeight * currentLength;
-        if (currentArea > maxArea) {
-          maxArea = currentArea;
-        }
+    int leftBoundaryPointer = 0;
+    int rightBoundaryPointer = height.length - 1;
+    while(leftBoundaryPointer < rightBoundaryPointer) {
+      int currentHeight = min(height[leftBoundaryPointer], height[rightBoundaryPointer]);
+      int currentLength = rightBoundaryPointer - leftBoundaryPointer;
+      int currentArea = currentHeight * currentLength;
+      if(currentArea > maxArea) {
+        maxArea = currentArea;
+      }
+      if(height[leftBoundaryPointer] > height[rightBoundaryPointer]) {
+        rightBoundaryPointer--;
+      } else {
+        leftBoundaryPointer++;
       }
     }
     return maxArea;
