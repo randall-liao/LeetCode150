fcfcbf270fefa4999cef7545178d009f096b290a
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2024-12-03 21:21:26 -0500

    feat: 649. Dota2 Senate double queue implementation and tests

--- src/main/java/leetcode75/queue/Dota2Senate.java
+++ src/main/java/leetcode75/queue/Dota2Senate.java
@@ -1,6 +1,6 @@
 package leetcode75.queue;
 
-import java.util.Iterator;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -9,7 +9,6 @@ public class Dota2Senate {
   public static String predictPartyVictory(String senate) {
     char[] senates = senate.toCharArray();
     int senatesLength = senates.length;
-    boolean[] isBanned = new boolean[senatesLength];
     Queue<Integer> radiantIndexVotingQueue = new LinkedList<>();
     Queue<Integer> direIndexVotingQueue = new LinkedList<>();
     for (int i = 0; i < senatesLength; i++) {
@@ -22,46 +21,16 @@ public class Dota2Senate {
           break;
       }
     }
-    int pointer = 0;
     while (!radiantIndexVotingQueue.isEmpty() && !direIndexVotingQueue.isEmpty()) {
-      if (!isBanned[pointer]) {
-        // When senate can vote, it will ban the next senate in opposite party
-        switch (senates[pointer]) {
-          case 'R':
-            Iterator<Integer> iterator = direIndexVotingQueue.iterator();
-            boolean isVoted = false;
-            while (iterator.hasNext()) {
-              Integer index = iterator.next();
-              if (index > pointer) {
-                isBanned[index] = true;
-                iterator.remove();
-                isVoted = true;
-                break;
-              }
-            }
-            if (!isVoted && !direIndexVotingQueue.isEmpty()) {
-              isBanned[direIndexVotingQueue.remove()] = true;
-            }
-            break;
-          case 'D':
-            Iterator<Integer> iteratorD = radiantIndexVotingQueue.iterator();
-            boolean isVotedD = false;
-            while (iteratorD.hasNext()) {
-              Integer index = iteratorD.next();
-              if (index > pointer) {
-                isBanned[index] = true;
-                iteratorD.remove();
-                isVotedD = true;
-                break;
-              }
-            }
-            if (!isVotedD && !radiantIndexVotingQueue.isEmpty()) {
-              isBanned[radiantIndexVotingQueue.remove()] = true;
-            }
-            break;
-        }
+      Integer radiantIndex = radiantIndexVotingQueue.remove();
+      Integer direIndex = direIndexVotingQueue.remove();
+      if (radiantIndex < direIndex) {
+        // The smaller index can vote first, and remove the other one by adding itself back to the
+        // queue.
+        radiantIndexVotingQueue.add(radiantIndex + senatesLength);
+      } else {
+        direIndexVotingQueue.add(direIndex + senatesLength);
       }
-      pointer = loopingArray(pointer, senatesLength);
     }
     if (radiantIndexVotingQueue.isEmpty()) {
       return "Dire";
@@ -70,11 +39,4 @@ public class Dota2Senate {
     }
   }
 
-  private static int loopingArray(int pointer, int arrayLength) {
-    if (pointer == arrayLength - 1) {
-      return 0;
-    } else {
-      return ++pointer;
-    }
-  }
 }
