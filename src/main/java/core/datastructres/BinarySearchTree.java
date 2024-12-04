28831b23753ee9953c2e629eadaf30ea56257b55
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2024-12-04 00:15:20 -0500

    feat: Binary Search Tree skeleton implementation

--- src/main/java/core/datastructres/BinarySearchTree.java
+++ src/main/java/core/datastructres/BinarySearchTree.java
@@ -1,36 +1,63 @@
 package core.datastructres;
 
+import java.util.Queue;
 
 public class BinarySearchTree<Key extends Comparable<Key>, Value> {
 
   private Node root;
 
-  public void put (Key key, Value val) {
-
-  }
+  public void put(Key key, Value val) {}
 
   public Value get(Key key) {
-      return null;
+    return null;
   }
 
-  public void delete(Key key) {
+  public void delete(Key key) {}
 
+  public Key getMin() {
+    return null;
+  }
+
+  public Key getMax() {
+    return null;
+  }
+
+  public Key floor(Key key) {
+    return null;
+  }
+
+  public Key ceiling(Key key) {
+    return null;
+  }
+
+  public Integer rank(Key key) {
+    return -1;
+  }
+
+  private Integer rank(Key key, Node x) {
+    return -1;
   }
 
   public Iterable<Key> iterator() {
-      return null;
+    return null;
   }
 
-  private static class Node<Key, Value>{
+  private void inorder(Node node, Queue<Key> queue) {}
 
-    Key key;
-    Value val;
-    Node<Key, Value> left, right;
+  private Node delete(Node node, Key key) {
+    return null;
+  }
+
+  private class Node {
+
+    private final Key key;
+    private final Value val;
+    private Node left, right;
+    private Integer count;
 
     Node(Key key, Value val) {
       this.key = key;
       this.val = val;
     }
   }
-
 }
