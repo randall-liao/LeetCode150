ff75fe4a722dd2d327053bfe430905bc8780461c
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2025-01-05 16:21:37 -0500

    feat: Adding new constructor to 'TreeNode'
    - Implement a constructor that takes an integer array as input of the tree

--- src/main/java/leetcode75/binarytreedfs/TreeNode.java
+++ src/main/java/leetcode75/binarytreedfs/TreeNode.java
@@ -1,5 +1,8 @@
 package leetcode75.binarytreedfs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {
   int val;
   TreeNode left;
@@ -15,5 +18,38 @@ public class TreeNode {
     this.val = val;
     this.left = left;
     this.right = right;
+  }
+
+  public TreeNode(Integer[] values) {
+    if (values == null || values.length == 0) {
+      return;
+    }
+    this.val = values[0];
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(this);
+    int index = 1;
+
+    while (!queue.isEmpty() && index < values.length) {
+      TreeNode current = queue.poll();
+
+      // Process left child
+      if (index < values.length && values[index] != null) {
+        current.left = new TreeNode(values[index]);
+        queue.offer(current.left);
+      }
+      index++;
+
+      // Process right child
+      if (index < values.length && values[index] != null) {
+        current.right = new TreeNode(values[index]);
+        queue.offer(current.right);
+      }
+      index++;
+    }
+  }
+
+  public static void main(String[] args) {
+    Integer[] values = {3, 1, 4, 3, null, 1, 5};
+    TreeNode root = new TreeNode(values);
   }
 }
