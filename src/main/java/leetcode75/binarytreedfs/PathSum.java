c78970bacd47a86f8c457711e471a6886f869263
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2025-02-10 16:20:42 -0500

    feat: Add implementation for Path Sum and tests

---
+++ src/main/java/leetcode75/binarytreedfs/PathSum.java
@@ -0,0 +1,19 @@
+package leetcode75.binarytreedfs;
+
+public class PathSum {
+  public boolean hasPathSum(TreeNode root, int targetSum) {
+    return sum(root, 0, targetSum);
+  }
+
+  private boolean sum(TreeNode node, int currentSum, int targetSum) {
+    if (node == null) {
+      return false;
+    }
+    currentSum += node.val;
+    // when DFS reach leaf node, compare
+    if (node.left == null && node.right == null) {
+      return currentSum == targetSum;
+    }
+    return (sum(node.left, currentSum, targetSum) || sum(node.right, currentSum, targetSum));
+  }
+}
---
+++ src/test/java/leetcode75/binarytreedfs/PathSumTest.java
@@ -0,0 +1,18 @@
+package leetcode75.binarytreedfs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PathSumTest {
+
+  PathSum pathSum = new PathSum();
+
+  @Test
+  void pathSum_exampleOne() {
+    TreeNode treeRoot =
+        new TreeNode(new Integer[] {5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1});
+    Integer targetSum = 22;
+    assertTrue(pathSum.hasPathSum(treeRoot, targetSum));
+  }
+}
