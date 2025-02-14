3434783854467caee941cb94bda84201c3c3ee15
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2025-02-13 23:41:25 -0500

    feat: implementation for Lowest Common Ancestor of a Binary Tree and its tests.

---
+++ src/main/java/leetcode75/binarytreedfs/LowestCommonAncestorBinaryTree.java
@@ -0,0 +1,19 @@
+package leetcode75.binarytreedfs;
+
+import java.util.Objects;
+
+public class LowestCommonAncestorBinaryTree {
+
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (root == null || root.val == p.val || root.val == q.val) {
+      return root;
+    }
+    TreeNode leftNode = lowestCommonAncestor(root.left, p, q);
+    TreeNode rightNode = lowestCommonAncestor(root.right, p, q);
+    if (leftNode == null && rightNode == null) {
+      return null;
+    } else if (leftNode != null && rightNode != null) {
+      return root;
+    } else return Objects.requireNonNullElse(leftNode, rightNode);
+  }
+}
---
+++ src/test/java/leetcode75/binarytreedfs/LowestCommonAncestorBinaryTreeTest.java
@@ -0,0 +1,46 @@
+package leetcode75.binarytreedfs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LowestCommonAncestorBinaryTreeTest {
+
+  LowestCommonAncestorBinaryTree lowestCommonAncestorBinaryTree;
+
+  @BeforeEach
+  void contextInit() {
+    lowestCommonAncestorBinaryTree = new LowestCommonAncestorBinaryTree();
+  }
+
+  @Test
+  void lowestCommonAncestor_exampleOne() {
+    TreeNode root = new TreeNode(new Integer[] {3, 5, 1, 6, 2, 0, 8, null, null, 7, 4});
+    TreeNode pNode = new TreeNode(5);
+    TreeNode qNode = new TreeNode(1);
+    int expected = 3;
+    assertEquals(
+        expected, lowestCommonAncestorBinaryTree.lowestCommonAncestor(root, pNode, qNode).val);
+  }
+
+  @Test
+  void lowestCommonAncestor_exampleTwo() {
+    TreeNode root = new TreeNode(new Integer[] {3, 5, 1, 6, 2, 0, 8, null, null, 7, 4});
+    TreeNode pNode = new TreeNode(5);
+    TreeNode qNode = new TreeNode(4);
+    int expected = 5;
+    assertEquals(
+        expected, lowestCommonAncestorBinaryTree.lowestCommonAncestor(root, pNode, qNode).val);
+  }
+
+  @Test
+  void lowestCommonAncestor_exampleThree() {
+    TreeNode root = new TreeNode(new Integer[] {1, 2});
+    TreeNode pNode = new TreeNode(1);
+    TreeNode qNode = new TreeNode(2);
+    int expected = 1;
+    assertEquals(
+        expected, lowestCommonAncestorBinaryTree.lowestCommonAncestor(root, pNode, qNode).val);
+  }
+}
