0f57780a3e1a902fe683d7341712c38847025cf7
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2025-02-26 23:11:28 -0500

    chore: 1466. Reorder Routes to Make All Paths Lead to the City Zero java implementation with tests

--- src/main/java/leetcode75/graphs/dfs/ReorderRoutesToCityZero.java
+++ src/main/java/leetcode75/graphs/dfs/ReorderRoutesToCityZero.java
@@ -2,73 +2,67 @@ package leetcode75.graphs.dfs;
 
 import java.util.*;
 
-class Edge {
-  int source;
-  int destination;
-  boolean isOriginalDirection;
+public class ReorderRoutesToCityZero {
 
-  public Edge(int source, int destination, boolean isOriginalDirection) {
-    this.source = source;
-    this.destination = destination;
-    this.isOriginalDirection = isOriginalDirection;
-  }
-
-  @Override
-  public String toString() {
-    return "("
-        + source
-        + " -> "
-        + destination
-        + ", originalDirection: "
-        + isOriginalDirection
-        + ")";
-  }
-}
-
-// Define a class to represent the graph
-class Graph {
-  private final Map<Integer, List<Edge>> adjacencyList;
-
-  public Graph() {
-    this.adjacencyList = new HashMap<>();
-  }
-
-  // Add an edge to the graph
-  public void addEdge(int source, int destination, boolean isOriginalDirection) {
-    this.adjacencyList.putIfAbsent(source, new ArrayList<>());
-    this.adjacencyList.get(source).add(new Edge(source, destination, isOriginalDirection));
-  }
-
-  // Get the adjacency list of the graph
-  public Map<Integer, List<Edge>> getAdjacencyList() {
-    return this.adjacencyList;
-  }
-
-  // Perform DFS traversal to calculate changes
-  public int dfs(int node, boolean[] visited) {
-    int changes = 0;
-    visited[node] = true;
-    for (Edge edge : adjacencyList.getOrDefault(node, new ArrayList<>())) {
-      int next = edge.destination;
-      if (!visited[next]) {
-        if (edge.isOriginalDirection) {
-          changes++;
+  public int minReorder(int n, int[][] connections) {
+    Graph citiesGraph = new Graph();
+    for (int[] edge : connections) {
+      citiesGraph.addEdge(edge[0], edge[1], true);
+      citiesGraph.addEdge(edge[1], edge[0], false);
+    }
+    boolean[] isCityVisited = new boolean[n];
+    Arrays.fill(isCityVisited, false);
+    // BFS starting from city zero
+    Stack<Edge> edgesStack = new Stack<>();
+    Map<Integer, List<Edge>> adjacencyList = citiesGraph.getAdjacencyList();
+    for (Edge edge : adjacencyList.getOrDefault(0, new ArrayList<>())) {
+      edgesStack.push(edge);
+    }
+    isCityVisited[0] = true;
+    int edgeNeedChange = 0;
+    while (!edgesStack.isEmpty()) {
+      Edge currentEdge = edgesStack.pop();
+      int nextDestination = currentEdge.destination;
+      if (!isCityVisited[nextDestination]) {
+        isCityVisited[nextDestination] = true;
+        if (currentEdge.isOriginalDirection) {
+          edgeNeedChange++;
         }
-        changes += dfs(next, visited);
+        for (Edge edge : adjacencyList.getOrDefault(nextDestination, new ArrayList<>())) {
+          edgesStack.push(edge);
+        }
       }
     }
-    return changes;
+    return edgeNeedChange;
   }
-}
 
-class ReorderRoutesToCityZero {
-  public int minReorder(int n, int[][] connections) {
-    Graph graph = new Graph();
-    for (int[] connection : connections) {
-      graph.addEdge(connection[0], connection[1], true); // Original direction
-      graph.addEdge(connection[1], connection[0], false); // Reverse direction
+  class Edge {
+
+    final int source;
+
+    final int destination;
+
+    final boolean isOriginalDirection;
+
+    Edge(int source, int destination, boolean isOriginalDirection) {
+      this.source = source;
+      this.destination = destination;
+      this.isOriginalDirection = isOriginalDirection;
     }
-    boolean[] visited = new boolean[n];
-    return graph.dfs(0, visited);
   }
+
+  class Graph {
+
+    final Map<Integer, List<Edge>> adjacencyList = new HashMap<>();
+
+    void addEdge(int source, int destination, boolean isOriginalDirection) {
+      this.adjacencyList.putIfAbsent(source, new ArrayList<>());
+      this.adjacencyList.get(source).add(new Edge(source, destination, isOriginalDirection));
+    }
+
+    Map<Integer, List<Edge>> getAdjacencyList() {
+      return this.adjacencyList;
+    }
+  }
+
 }
